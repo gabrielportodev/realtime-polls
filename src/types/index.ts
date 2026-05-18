152a52b47ea4d14c70c8ab69547834029ad202bf
Gabriel Porto <gpalmenara@gmail.com>
2026-05-18 00:02:00 -0300

    feat: definir tipos base da aplicação de enquetes

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+export type PollStatus = 'not_started' | 'started' | 'ongoing' | 'finished'
+
+export type PollOption = {
+  id: string
+  poll_id: string
+  text: string
+  votes?: Vote[]
+}
+
+export type Poll = {
+  id: string
+  title: string
+  start_at: string
+  end_at: string
+  created_at: string
+  poll_options?: PollOption[]
+}
+
+export type Vote = {
+  id: string
+  poll_option_id: string
+  created_at: string
+}
